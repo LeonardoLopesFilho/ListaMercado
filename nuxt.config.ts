ca93f4a8a35c9edb305c39c4f85858da948450e8
LeonardoLopesFilho <leo_zinho_filho@hotmail.com>
2025-07-04 11:49:28 -0300

    feat/adicionado pwa e tirado a tradução automatica

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,19 @@
 import vuetify from 'vite-plugin-vuetify'
 
 export default defineNuxtConfig({
+  app: {
+    head: {
+      htmlAttrs: {
+        lang: 'pt-BR', 
+      },
+      meta: [
+        { name: 'google', content: 'notranslate' }, 
+      ],
+    },
+  },
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/icon'],
+  modules: ['@nuxt/eslint', '@nuxt/icon', '@vite-pwa/nuxt'],
   css: ['vuetify/styles'],
   build: {
     transpile: ['vuetify'],
@@ -12,5 +22,32 @@ export default defineNuxtConfig({
     plugins: [
       vuetify({ autoImport: true }),
     ],
+  },
+  pwa: {
+    registerType: 'autoUpdate',
+    manifest: {
+      name: 'Lista de Mercado',
+      short_name: 'ListaMercado',
+      description: 'Aplicação para auxiliar na gestão de compras',
+      theme_color: '#ffffff',
+      background_color: '#ffffff',
+      display: 'standalone',
+      start_url: '/',
+      icons: [
+        {
+          src: '/retail.png',
+          sizes: '192x192',
+          type: 'image/png',
+        },
+        {
+          src: '/retail.png',
+          sizes: '512x512',
+          type: 'image/png',
+        },
+      ],
+    },
+    workbox: {
+      /* Configurações do service worker, se quiser */
+    },
   },
 })
